68ea7e01f26f6e381b0f9f67bb7cfdccfcf62b4a
HXACA <718531794@qq.com>
2018-01-16 16:02:20 +0800

    2018/1/16

--- 朴素贝叶斯/bayes.py
+++ 朴素贝叶斯/bayes.py
@@ -97,8 +97,13 @@ def bagOfWords2VecMN(vocabList, inputSet):
 def textParse(bigString):
     #文本切分
     import re
-    listOfTokens = re.split(r'\W*',bigString)
+    #listOfTokens = re.split(r'\W*',bigString)
     #正则表达式，只分割字母，数字
+    xx = u"([\u4e00-\u9fa5]+)"
+    pattern = re.compile(xx)
+    listOfTokens = pattern.findall(bigString)
+    #中文的正则
+   # print listOfTokens
     return [tok.lower() for tok in listOfTokens if len(tok)>2]
     #转小写并只选择长度大于2的
 
@@ -141,4 +146,85 @@ def spamTest():
             errorCount+=1
     print 'the error rate is : ',float(errorCount)/len(testSet)
 
+def calcMostFreq(vocabList,fullText):
+    #计算频率
+    import operator
+    freqDict = {}
+    for token in vocabList:
+        freqDict[token] = fullText.count(token)
+    sortedFreq = sorted(freqDict.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedFreq[:30]
+    #返回频率最高的前30个单词
 
+def localWords(feed1,feed0):
+    #获取区域趋向
+    import feedparser
+    doclist = []
+    classlist = []
+    fullText = []
+    minLen = min(len(feed1['entries']),len(feed0['entries']))
+    for i in range(minLen):
+        wordList = textParse(feed1['entries'][i]['summary'])
+        #print feed1['entries'][i]['summary']
+        doclist.append(wordList)
+        fullText.append(wordList)
+        classlist.append(1)
+        wordList = textParse(feed0['entries'][i]['summary'])
+        #print feed0['entries'][i]['summary']
+        doclist.append(wordList)
+        fullText.append(wordList)
+        classlist.append(0)
+    vocabList = createVocabList(doclist)
+    top30Words = calcMostFreq(vocabList, fullText)
+    for pairW in top30Words:
+        if pairW[0] in vocabList:
+            vocabList.remove(pairW[0])
+            #去除频率最高的词
+    trainingSet = range(2*minLen)
+    testSet = []
+    for i in range(20):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+    trainMat = []
+    trainClasses = []
+    for docIndex in trainingSet:
+        trainMat.append(bagOfWords2VecMN(vocabList,doclist[docIndex]))
+        trainClasses.append(classlist[docIndex])
+    p0V,p1V,pSam = trainNB0(trainMat,trainClasses)
+    errorCount = 0
+    for docIndex in testSet:
+        wordVect = bagOfWords2VecMN(vocabList,doclist[docIndex])
+        if classifyNB(wordVect,p0V,p1V,pSam)!=classlist[docIndex]:
+            errorCount+=1
+    print 'the error rate is : ' ,float(errorCount)/len(testSet)
+    return vocabList,p0V,p1V
+
+def getTopWords(ny,sf):
+    import operator
+    vocabList,p0V,p1V = localWords(ny, sf)
+    #p0V : SF p1V:NY
+    topNY = []
+    topSF = []
+    for i in range(len(p0V)):
+        if p0V[i]> -6.0:
+            topSF.append((vocabList[i],p0V[i]))
+        if(p1V[i]> -6.0):
+            topNY.append((vocabList[i],p1V[i]))
+            #根据概率加入特定集
+    sortedSF = sorted(topSF,key=lambda pair:pair[1],reverse=True)
+    #按照出现概率排序
+    count = 0
+    for item in sortedSF:
+        print item[0]
+        count+=1
+        if count>10:
+            break
+    print '*********************************************************************'
+    count = 0
+    sortedNY = sorted(topNY, key=lambda pair: pair[1], reverse=True)
+    for item in sortedNY:
+        print item[0]
+        count += 1
+        if count > 10:
+            break
