12fa7b1ec97fe8f073476e763e66c3d9b33e01ce
HXACA <718531794@qq.com>
2018-01-20 22:14:51 +0800

    2018/1/20

--- Logistic回归/logRegres.py
+++ Logistic回归/logRegres.py
@@ -13,10 +13,13 @@ def loadDataSet():
     for line in fr.readlines():
         lineArr = line.strip().split()
         dataMat.append([1.0,float(lineArr[0]),float(lineArr[1])])
+        #常数项+参数项
         labelMat.append(int(lineArr[2]))
+        #数据集label
     return dataMat,labelMat
 
 def sigmoid(inX):
+    #一个神奇的函数
     return 1.0/(1+exp(-inX))
 
 def gradAscent(dataMatIn,classLabels):
@@ -25,7 +28,9 @@ def gradAscent(dataMatIn,classLabels):
     #转置
     m,n = shape(dataMatrix)
     alpha = 0.001
+    #步长
     maxCycles = 500
+    #迭代次数
     weights = ones((n,1))
     #系数矩阵
     for k in range(maxCycles):
@@ -34,3 +39,95 @@ def gradAscent(dataMatIn,classLabels):
         weights = weights + alpha*dataMatrix.transpose()*error
         #按照差值方向调整回归系数
     return weights
+
+def plotBestFit(weights):
+    #绘图
+    import matplotlib.pyplot as plt
+    dataMat,labelMat = loadDataSet()
+    dataArr = array(dataMat)
+    weights = array(weights)
+    n = shape(dataMat)[0]
+    xcord1 = [];ycord1 = []
+    xcord2 = [];ycord2 = []
+    for i in range(n):
+        if int(labelMat[i])==1:
+            xcord1.append(dataArr[i,1]);ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1]);ycord2.append(dataArr[i,2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1,ycord1,s=30,c='red',marker='s')
+    ax.scatter(xcord2,ycord2,s=30,c='green')
+    x = arange(-3.0,3.0,0.1)
+    y = (-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x,y)
+    plt.xlabel('X1');plt.ylabel('X2')
+    plt.show()
+
+def stocGradAscent0(dataMatrix,classLabels):
+    #随机梯度上升
+    m,n=shape(dataMatrix)
+    alpha = 0.01
+    weights = ones(n)
+    for i in range(m):
+        h = sigmoid(sum(dataMatrix[i]*weights))
+        error = classLabels[i]-h
+        weights = weights+alpha*error*dataMatrix[i]
+    return weights
+
+def stocGradAscent1(dataMatrix,classLabels,numIter=150):
+    #改进的随机梯度上升
+    m,n=shape(dataMatrix)
+    weights = ones(n)
+    for j in range(numIter):
+        dataIndex = range(m)
+        for i in range(m):
+            alpha = 4/(1.0+i+j)+0.01
+            randIndex = int(random.uniform(0,len(dataIndex)))
+            #随机选择更新
+            h = sigmoid(sum(dataMatrix[randIndex]*weights))
+            error = classLabels[randIndex]-h
+            weights = weights+error*alpha*dataMatrix[randIndex]
+            del(dataIndex[randIndex])
+    return weights
+
+def classifyVector(inX,weights):
+    prob = sigmoid(sum(inX*weights))
+    if(prob>0.5):return 1.0
+    else:
+        return 0.0
+
+def colicTest():
+    frTrain = open('horseColicTraining.txt')
+    frTest = open('horseColicTest.txt')
+    trainingSet = []
+    trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(len(currLine)-1):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    #print trainingSet,trainingLabels
+    trainWeights = stocGradAscent1(array(trainingSet),trainingLabels,500)
+    errorCount = 0.0
+    numTestVect = 0.0
+    for line in frTest.readlines():
+        numTestVect+=1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(len(currLine)-1):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(array(lineArr),trainWeights)) != int(currLine[21]):
+            errorCount+=1
+    errorRate = errorCount/numTestVect
+    print u"错误率是：%f" %errorRate
+    return errorRate
+
+def multiTest():
+    numTests = 10
+    errorSum = 0.0
+    for k in range(numTests):
+        errorSum+=colicTest()
+    print u"在%d次测试后，平均错误次数率是：%f" %(numTests,errorSum/numTests)
